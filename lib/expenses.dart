ebb26277c73e539a5c5ed772a4e24ca12d39738a
Bhawesh <bhaweshbaruwal3@gmail.com>
2025-10-06 19:42:23 +0545

    initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,52 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/new_expense.dart';
+import "package:flutter/material.dart";
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 1800,
+        date: DateTime.now(),
+        category: Category.education),
+    Expense(
+        title: 'Travel',
+        amount: 40,
+        date: DateTime.now(),
+        category: Category.travel),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bhawesh Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('This is for chart..'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
